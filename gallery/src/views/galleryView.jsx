2ff7cc3a41225da0fc0e4d5f706510dbd4b4be34
tundraElie <tundra.elie@gmail.com>
2024-07-22 02:11:05 -0400

    web scrapping of unsplash en retart and data to use, icons and load

--- gallery/src/views/galleryView.jsx
+++ gallery/src/views/galleryView.jsx
@@ -1,11 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import useMediaQuery from '@mui/material/useMediaQuery';
 import {
-  Container, Paper, Card, Grid,ImageList,ImageListItem,Box,Chip,IconButton,
-  List, Button, Stack, ListItem, ImageContainer, ListItemIcon, ListItemText,ImageListItemBar,
+  Container, Paper, Card, Grid,ImageList,ImageListItem,Box,Chip,IconButton, useTheme, Avatar,
+  List, Button, Stack, ListItem, ImageContainer, ListItemIcon, ListItemText,ImageListItemBar, CircularProgress,
+  Typography,
 } from '@mui/material';
 import { styled, createTheme, ThemeProvider } from '@mui/material/styles';
 import AcUnitIcon from '@mui/icons-material/AcUnit';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
+import AddIcon from '@mui/icons-material/Add';
+import data_unsplash from '../data/data_unsplash.json'
 
 const theme = createTheme();
 
@@ -22,7 +27,7 @@ const Root = styled('div')(({ theme }) => ({
 }));
 
 const Image = styled('img')(({ theme }) => ({
-  borderRadius: 20,
+  borderRadius: 5,
   transition: 'opacity 0.3s ease',
 }));
 
@@ -58,14 +63,46 @@ const BottomRow = styled(IconRow)(({ theme }) => ({
   left: 0,
 }));
 
+const OutlinedIconButton = styled(IconButton)(({ theme }) => ({
+  // border: `1px solid ${theme.palette.text.primary}`,
+  borderRadius: theme.shape.borderRadius,
+  padding: 0,
+  margin: 5,
+  color: "#767676",
+  backgroundColor: "#EEEEEE !important",
+  alignItems: 'center',
+  justifyContent: 'center',
+  display: 'flex',
+  width: 40, // Fixed width
+  height: 40, // Fixed height
+}));
+
 export default function GalleryView() {
-  const [visibleImages, setVisibleImages] = useState(itemData.slice(0, 6));
-  const matches = useMediaQuery(theme.breakpoints.down('sm'));
+  const [visibleImages, setVisibleImages] = useState(data_unsplash.slice(0, 6));
+  const [loading, setLoading] = useState(false);
+
   const handleLoadMore = () => {
+    setLoading(true);
     const currentLength = visibleImages.length;
-    const nextImages = itemData.slice(currentLength, currentLength + 30);
-    setVisibleImages([...visibleImages, ...nextImages]);
+    const nextImages = data_unsplash.slice(currentLength, currentLength + 20);
+    setTimeout(() => {
+      setVisibleImages([...visibleImages, ...nextImages]);
+      setLoading(false);
+    }, 1000); // Simulate a network request with timeout
   };
+
+  const theme = useTheme();
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm')); // Small screens (mobile)
+  const isMediumScreen = useMediaQuery(theme.breakpoints.down('md')); // Medium screens (tablet)
+
+  let cols;
+  if (isSmallScreen) {
+    cols = 1;
+  } else if (isMediumScreen) {
+    cols = 2;
+  } else {
+    cols = 3;
+  }
 
   return (
     <Box sx={{ 
@@ -76,12 +113,12 @@ export default function GalleryView() {
       alignItems: 'center',
       }}>
       <Box>
-        <ImageList variant="masonry" cols={matches ? 1 : 3} gap={15}>
-          {itemData.map((image, index) => (
+        <ImageList variant="masonry" cols={cols} gap={15}>
+          {visibleImages.map((image, index) => (
             <ThemeProvider theme={theme}>
             <Root>
             <ImageListItem key={index}>
-              <Image  className="image"
+              <Image className="image"
                 srcSet={`${image.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
                 src={`${image.img}?w=248&fit=crop&auto=format`}
                 alt={`Img${index}`}
@@ -89,21 +126,29 @@ export default function GalleryView() {
                 style={{ width: '100%', height: 'auto' }}
                 />
                 <Overlay className="overlay">
-                  <TopRow direction="row">
-                    <IconButton>
-                      <AcUnitIcon /><AcUnitIcon />
-                    </IconButton>
-                    <IconButton>
-                      <AcUnitIcon />
-                    </IconButton>
-                  </TopRow>
+                <TopRow direction="row">
+                  <IconButton>
+                    <AcUnitIcon />
+                  </IconButton>
+                  <div style={{ display: 'flex', gap: 1 }}>
+                    <OutlinedIconButton>
+                      <FavoriteIcon />
+                    </OutlinedIconButton>
+                    <OutlinedIconButton>
+                      <AddIcon />
+                    </OutlinedIconButton>
+                  </div>
+                </TopRow>
                   <BottomRow direction="row">
                     <IconButton>
-                      <AcUnitIcon /> Life really sucks
+                      <Avatar sx={{marginRight: 2}} alt={image.author} src={image.imgUser}/>
+                    <Typography sx={{color: '#FFF'}}>
+                    {image.author}
+                    </Typography>
                     </IconButton>
-                    <IconButton>
-                      <AcUnitIcon />
-                    </IconButton>
+                    <OutlinedIconButton>
+                      <ArrowDownwardIcon />
+                    </OutlinedIconButton>
                   </BottomRow>
                 </Overlay>
                 <Stack direction="row" spacing={1} sx={{ margin: 1 }}>
@@ -116,135 +161,13 @@ export default function GalleryView() {
             </ThemeProvider>
           ))}
         </ImageList>
+        {loading && (
+        <Box display="flex" justifyContent="center" my={2}>
+          <CircularProgress />
+        </Box>
+      )}
       <Button sx= {{width: '100%', marginTop: 5}}variant="contained" onClick={handleLoadMore}> Más🌉</Button>
       </Box>
     </Box>
   );
 };
-
-// const useStyles = makeStyles(() => ({
-//   root: {
-//     position: 'relative',
-//     display: 'inline-block',
-//     '&:hover $overlay': {
-//       opacity: 1,
-//     },
-//     '&:hover $image': {
-//       opacity: 0.7,
-//     },
-//   },
-//   image: {
-//     display: 'block',
-//     width: '100%',
-//     height: 'auto',
-//     transition: 'opacity 0.3s ease',
-//   },
-//   overlay: {
-//     position: 'absolute',
-//     top: 0,
-//     left: 0,
-//     width: '100%',
-//     height: '100%',
-//     opacity: 0,
-//     display: 'flex',
-//     flexDirection: 'column',
-//     justifyContent: 'space-between',
-//     transition: 'opacity 0.3s ease',
-//     padding: 0, // Remove padding
-//   },
-//   iconRow: {
-//     display: 'flex',
-//     justifyContent: 'space-between',
-//     width: '100%',
-//     padding: 0, // Remove padding
-//   },
-//   topRow: {
-//     position: 'absolute',
-//     top: 0,
-//     left: 0,
-//     width: '100%',
-//     padding: 8, // Adjust if needed
-//   },
-//   bottomRow: {
-//     position: 'absolute',
-//     bottom: 0,
-//     left: 0,
-//     width: '100%',
-//     padding: 8, // Adjust if needed
-//   },
-// }));
-
-const itemData = [
-  {
-    img: 'https://images.unsplash.com/photo-1549388604-817d15aa0110',
-    title: 'Bed',
-    author: 'swabdesign',
-    tags: ['HD Wallpaper', 'background']
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1525097487452-6278ff080c31',
-    title: 'Books',
-    author: 'Pavel Nekoranec',
-    tags: ['HD Wallpaper', 'background']
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1523413651479-597eb2da0ad6',
-    title: 'Sink',
-    author: 'Charles Deluvio',
-    tags: ['HD Wallpaper', 'background']
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1563298723-dcfebaa392e3',
-    title: 'Kitchen',
-    author: 'Christian Mackie',
-    tags: ['HD Wallpaper', 'View']
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1588436706487-9d55d73a39e3',
-    title: 'Blinds',
-    author: 'Darren Richardson',
-    tags: ['View']
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1574180045827-681f8a1a9622',
-    title: 'Chairs',
-    author: 'Taylor Simpson',
-    tags: ['HD Wallpaper']
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1530731141654-5993c3016c77',
-    title: 'Laptop',
-    author: 'Ben Kolde',
-    tags: ['HD Wallpaper', 'View']
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1481277542470-605612bd2d61',
-    title: 'Doors',
-    author: 'Philipp Berndt',
-    tags: ['Photography']
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1517487881594-2787fef5ebf7',
-    title: 'Coffee',
-    author: 'Jen P.',
-    tags: ['HD Wallpaper', 'Photography']
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1516455207990-7a41ce80f7ee',
-    title: 'Storage',
-    author: 'Douglas Sheppard',
-    tags: ['HD Wallpaper', 'View']
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1597262975002-c5c3b14bbd62',
-    title: 'Candle',
-    author: 'Fi Bell',
-    tags: ['HD Wallpaper', 'Photography']
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1519710164239-da123dc03ef4',
-    title: 'Coffee table',
-    author: 'Hutomo Abrianto',
-    tags: ['HD Wallpaper', 'Photography']
-  },
-];
